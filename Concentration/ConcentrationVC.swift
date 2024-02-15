108876723d9b2aa3b1b05a841974b0b88494f6b4
se8un <se8un@yandex.ru>
2024-02-15 05:43:45 +0300

    game v1 done git commiting

--- Concentration/ConcentrationVC.swift
+++ Concentration/ConcentrationVC.swift
@@ -8,22 +8,77 @@
 import UIKit
 
 class ConcentrationVC: UIViewController {
-
+    
+    lazy var game = Concentration(numberOfPairsOfCard: (cardButtons.count + 1) / 2)
+    
+    var flipCount = 0 { didSet { flipCountLabel.text = "Ход: \(flipCount)" } }
+    
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+        } else {
+            print("cardNumber is nil!")
+        }
     }
-    */
-
+    
+    
+    func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: .normal)
+                button.backgroundColor = .white
+            } else {
+                button.setTitle("", for: .normal)
+                button.backgroundColor = card.isMatched ? .clear : .randomColor()
+            }
+        }
+    }
+    
+    
+    var emojiChoices = ["🥝", "🥕", "🍔", "🪿", "🐥", "🍟", "🤗", "🎱", "❤️", "🐮", "🪵", "🍀", "🍄", "🔥", "🌕", "🍒", "🏀", "🧘", "🥇", "✈️", "🚀"]
+    
+    var emoji = [Int:String]()
+    
+    
+    func emoji(for card: Card) -> String {
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        }
+        return emoji[card.identifier] ?? "?"
+    }
+    
 }
+
+
+
+
+//_/_/_/_/_/_/_/_/ NOTE _/_/_/_/_/_/_/_/_/_/_/_/
+
+
+//            button.titleLabel?.font = UIFont.systemFont(ofSize:  50)
+
+
+// flipCard(emoji: cards[cardNumber], button: sender)
+
+
+//if emoji[card.identifier] != nil {
+//            return emoji[card.identifier]!
+//        } else {
+//            return "?"
+//        }
--- Concentration/UIColor+Ex.swift
+++ Concentration/UIColor+Ex.swift
@@ -5,4 +5,15 @@
 //  Created by de.egorov on 15.02.2024.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    
+    /// generate random RGB color with alpha=1
+    static func randomColor() -> UIColor {
+        UIColor(red:    .random(in:  0...1),
+                green:  .random(in:  0...1),
+                blue:   .random(in:  0...1),
+                alpha:  1)
+    }
+}
